6001c155f013024b7899913e9262de0dd13ec33e
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-19 01:34:29 +0400

    задача 6

---
+++ dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+string number1_str = Console.ReadLine();
+int number = Convert.ToInt32(number1_str);
+int a = number % 2;
+if (a == 0)
+{
+Console.Write("число " + number + " четное");
+}
+else
+{   
+     Console.WriteLine("число " + number + " нечетное");
+}
