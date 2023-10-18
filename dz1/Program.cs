e4b8588e23478d2b15bf1ee0485593f58ea71353
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-18 23:12:19 +0400

    Задача 2

---
+++ dz1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+string number1_str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1_str);
+Console.WriteLine("Введите второе число");
+string number2_str = Console.ReadLine();
+int number2 = Convert.ToInt32(number2_str);
+if (number1 > number2)
+{
+    Console.Write("max = " + number1);
+}
+if (number1 < number2)
+{
+    Console.Write("max=" + number2);
+}
+else
+{
+    Console.WriteLine("числа равны" + number1  + number2);
+}
---
+++ primer1/Program.cs
@@ -0,0 +1,8 @@
+﻿Random generatenumber = new Rundom();
+int number = generatenumber.Next(100,1000);
+Console.WriteLine(number)
+int numberA= number / 100;
+Console.WriteLine(numberA);
+int numberB= number % 10;
+Console.WriteLine(numberB);
+Console.WriteLine(numberA.Tostring() + numberB.Tostring()); //Console.WriteLine($"{b}" + $"{a}");
