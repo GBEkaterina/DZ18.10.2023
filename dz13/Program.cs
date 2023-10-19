4b1e85f9efd604428a48730814f5035d31a9d47d
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-19 13:15:05 +0400

    Задача 13

---
+++ dz13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string number1 = Console.ReadLine();
+int number = Convert.ToInt32(number1);
+if (number >= 100)
+{
+    string number2 = number1.ToString();
+    char thirdDigit = number2[2];
+    Console.WriteLine(thirdDigit);
+}
+else
+{
+     Console.WriteLine("третьего числа нет");
+}
