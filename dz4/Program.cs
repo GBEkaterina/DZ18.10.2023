48559d4b5ae81c55defd2617604045ecfcf0421e
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-19 10:29:05 +0400

    Задача 8

---
+++ dz4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string number1_str = Console.ReadLine();
+int number = Convert.ToInt32(number1_str);
+int i = 1;
+while(i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console. WriteLine( i );
+        i++;
+    }
+    i++;
+}
