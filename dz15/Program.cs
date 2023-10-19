2b294b4798d605cc529e723809cac547979c3d9e
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-19 15:41:07 +0400

    Задача 15

---
+++ dz15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число дня недели");
+string strnumber = Console.ReadLine();
+int number = Convert.ToInt32(strnumber);
+if(number >=1 && number <8)
+    if (number < 6 && number >= 1)
+    {   
+        Console.Write("Нет");
+    }
+    else
+    {
+        Console.Write("Да");
+    }
+else
+{
+    Console.Write("Ошибка ввода данных");
+}
