3b3e97b0d7013edea1e5ca434de921a72cae88af
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-19 11:03:42 +0400

    Задача 10

---
+++ dz10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >=100 && number < 1000)
+{
+    int Lastnumber = number % 100;
+    int secondnum = Lastnumber / 10;
+    Console. WriteLine(secondnum);
+}
+else
+{
+    Console.WriteLine("Введите число меньше 1000");
+}
